4370266d7fb212484675e3613621a70aac4f8142
TrollSmurfen <tannyhurainjinnath@gmail.com>
2026-01-22 15:50:47 +0100

    add lcd library and update other files according to it

--- pico_client/src/main.c
+++ pico_client/src/main.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
+#include "lcd.h"
+#include "hardware/i2c.h"
+#include "hardware/gpio.h"
 
 
 // Vi deklarerar dessa här uppe, men fyller dem med innehåll senare eller i en annan fil
@@ -10,25 +13,25 @@ void uppdatera_lcd(int betyg);
 
 
 int main() {
-    initiera_allt(); // Startar knappar, wifi, lcd
+ initiera_allt(); // Startar knappar, wifi, lcd
 
-    // "kontroll-loop"
-    while(1) {
-        // 1. Titta vilken knapp som trycks (returnerar 0 om ingen trycks, annars 1-5)
-        int betyg = las_av_knappar();
+// "kontroll-loop"
+while(1) {
+// 1. Titta vilken knapp som trycks (returnerar 0 om ingen trycks, annars 1-5)
+int betyg = las_av_knappar();
 
-        // Om vi faktiskt fick ett betyg (någon tryckte)
-        if (betyg > 0) {
-             printf("Betyg registrerat: %d\n", betyg);
+// Om vi faktiskt fick ett betyg (någon tryckte)
+if (betyg > 0) {
+printf("Betyg registrerat: %d\n", betyg);
 
              // 2. Skicka knapp-färg till databas/thinksspeak
-             skicka_till_thingspeak(betyg);
+            skicka_till_thingspeak(betyg);
 
              // 3. Uppdatera status på LCD-Skärm
-             uppdatera_lcd(betyg);
-             
+            uppdatera_lcd(betyg);
+            
              // Vänta lite så vi inte registrerar samma tryck flera gånger
-             sleep_ms(1000); 
+        sleep_ms(1000); 
         }
         
         sleep_ms(50); // Sparar ström
@@ -50,9 +53,23 @@ void skicka_till_thingspeak(int betyg) {
 
 void uppdatera_lcd(int betyg) {
     // Här lägger vi LCD-koden
+    lcd_clear();
+    lcd_set_cursor(0, 0);
+    lcd_print("Senaste betyg:");
+    lcd_set_cursor(1, 0);
+    char buf[16];
+    sprintf(buf, "%d", betyg);
+    lcd_print(buf);
 }
 
 void initiera_allt() {
     stdio_init_all();
-    // Här lägger vi gpio_init osv.
+    // Här lägger vi gpio_init osv.// Init I2C for LCD
+    i2c_init(i2c0, 100000);
+    gpio_set_function(0, GPIO_FUNC_I2C);
+    gpio_set_function(1, GPIO_FUNC_I2C);
+    gpio_pull_up(0);
+    gpio_pull_up(1);
+    lcd_init();
+
 }
